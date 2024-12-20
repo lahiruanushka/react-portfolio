91b6622a92040ed8876a2bca61f0ca36fe469950
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-12-20 11:46:18 +0530

    refactor(hero-section): remove scroll indicator

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -69,25 +69,6 @@ const Hero = () => {
           </motion.div>
         </div>
       </div>
-
-      {/* Scroll indicator */}
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center z-[3] pointer-events-none">
-        <a href="#about" className="pointer-events-auto">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="w-3 h-3 rounded-full bg-white mb-1"
-            />
-          </div>
-        </a>
-      </div>
     </section>
   );
 };
