10c0228205ecf41593074739ea05a67d74dc369f
Lahiru Anushka <lahiruanushkaofficial@gmail.com>
2025-07-31 19:05:17 +0800

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -252,7 +252,7 @@ const testimonials = [
       "I had the pleasure of working alongside Lahiru, who consistently demonstrated his passion for software development and problem solving. As a Trainee Software Engineer, Lahiru showcased exceptional dedication to learning and building innovative solutions. I am confident Lahiru's commitment to excellence and growth will continue to shine in his career. I strongly recommend him to any team or organization looking for a motivated and skilled software engineer.",
     name: "Ranju Laksahan Gamage",
     designation: "Software Engineer",
-    company: "DSP Engineering Solutions",
+    company: "DSP Engineering Solutions Pvt Ltd",
     image: ranjuLakshan,
   },
 ];
